ceaf13a2014a5b02b367c4e0e818c1fb82f620a3
Phillip Schichtel <phillip@schich.tel>
2018-08-25 20:19:49 +0200

    cleanup the build

--- src/main/kotlin/tel/schich/virtualscanner/screen.kt
+++ src/main/kotlin/tel/schich/virtualscanner/screen.kt
@@ -1,3 +1,20 @@
+/**
+ * virtual-scanner - Scan barcodes from your screen and emit the content as key strokes.
+ * Copyright © 2018 Phillip Schichtel (${email})
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package tel.schich.virtualscanner
 
 import java.awt.GraphicsEnvironment
